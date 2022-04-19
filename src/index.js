e01e815280321b8c72944d9bbc065f85809eabd7
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-19 20:42:04 -0300

    :lipstick: style: Aplicado componente de estlização global

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
