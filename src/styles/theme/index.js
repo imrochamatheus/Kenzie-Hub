0e2434ebecbec3311760c6fc5152749358601c6d
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-19 10:13:53 -0300

    :lipstick: style: Definido tema padrão da aplicação

---
+++ src/styles/theme/index.js
@@ -0,0 +1,18 @@
+export const theme = {
+  primary: {
+    main: "#FF577F",
+    focus: "#FF427F",
+    negative: "#59323F",
+  },
+  secondary: {
+    grey4: "#121214",
+    grey3: "#212529",
+    grey2: "#343B41",
+    grey1: "#868E96",
+    grey0: "#F8F9FA",
+  },
+  feedback: {
+    success: "#3FE864",
+    negative: "#E83F5B",
+  },
+};
