531d8a360132057ab47f3149035eaaffc1932e5e
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-19 20:34:57 -0300

    :sparkles: feat: Definidas rotas principais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Login />
+      </Route>
+      <Route path="/register" exact>
+        <Register />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
