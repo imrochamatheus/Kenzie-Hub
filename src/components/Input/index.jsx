8095b2d765d7d557c43ea53ed2bb695310939044
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-21 01:55:05 -0300

    :lipstick: style: Refatorado componente Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,42 +1,6 @@
 import Text from "../Text";
 import Box from "../Box";
-import styled from "styled-components";
-
-const StyledInput = styled.input`
-  width: 100%;
-  height: 38.5px;
-  color: white;
-  outline: none;
-  font-size: 13px;
-  font-weight: 400;
-  text-align: left;
-  line-height: 21px;
-  border-radius: 4px;
-  letter-spacing: 0em;
-  background: #343b41;
-  box-sizing: border-box;
-  border: 1px solid white;
-  padding: 0px 16px 0px 16px;
-  border: ${({ border }) => (border ? `1px solid ${border};` : "none")};
-`;
-
-const StyledSelect = styled.select`
-  width: 100%;
-  height: 38.5px;
-  color: white;
-  outline: none;
-  font-size: 13px;
-  font-weight: 400;
-  text-align: left;
-  line-height: 21px;
-  border-radius: 4px;
-  letter-spacing: 0em;
-  background: #343b41;
-  box-sizing: border-box;
-  border: 1px solid white;
-  padding: 0px 16px 0px 16px;
-  border: ${({ border }) => (border ? `1px solid ${border};` : "none")};
-`;
+import { StyledInput, StyledSelect } from "./styles";
 
 const Input = (props) => {
   return props.type !== "select" ? (
